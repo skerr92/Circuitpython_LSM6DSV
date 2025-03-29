a528885b10dce9229b5bf8c6da48ee678318e703
Seth Kerr <thesethkerr@gmail.com>
2025-03-29 15:52:09 -0600

    initial stuff

---
+++ examples/lsm6dsv_simpletest.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2025 Seth Kerr for Oak Dev Tech
+#
+# SPDX-License-Identifier: Unlicense
---
+++ lsm6dsv.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2025 Seth Kerr for Oak Dev Tech
+#
+# SPDX-License-Identifier: MIT
+"""
+`lsm6dsv`
+================================================================================
+
+A driver library for the LSM6DSV IMU from ST Electronics
+
+
+* Author(s): Seth Kerr
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/skerr92/CircuitPython_LSM6DSV.git"
